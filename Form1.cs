6caecdd7f342c9253fcd74bb138746c9de47701c
Руслан Вальшин <Ruslan376@outlook.com>
2020-02-23 19:58:11 +0500

    Version 1.1 - добавлен список файлов

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,7 +22,13 @@ namespace Draft_Audio_Player
         AudioFileReader fileReader;
         bool musicIsPlaying = false;
         string audioPath = "";
-        int timerInSeconds = 0;
+        string musicFolderPath = "";
+        string[] fileNames;
+        System.Windows.Forms.Panel[] musicListPanels;
+        System.Windows.Forms.Label[] musicListPanelsLabelNames;
+        System.Windows.Forms.Label[] musicListPanelsLabelDurations;
+        System.Windows.Forms.Button[] musicListPanelsButton;
+        System.Windows.Forms.CheckBox[] musicListPanelsCheckBox;
 
         public DraftAudioPlayerMainForm()
         {
@@ -37,7 +44,7 @@ namespace Draft_Audio_Player
                 musicIsPlaying = false;
                 timerOfPlayback.Enabled = false;
             }
-            else if(playButton.Text == "")
+            else if (playButton.Text == "")
             {
                 outputDevice.Play();
                 timerOfPlayback.Start();
@@ -92,6 +99,105 @@ namespace Draft_Audio_Player
         {
             timerOfPlayback.Enabled = false;
             durationOfPlayback.Text = (musicTrackBar.Value / 60).ToString("00") + ":" + (musicTrackBar.Value % 60).ToString("00");
+        }
+
+        private void directoryChooseButton_Click(object sender, EventArgs e)
+        {
+            
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                musicFolderPath = folderBrowserDialog.SelectedPath;
+                fileNames = Directory.GetFileSystemEntries(musicFolderPath, "*.mp3", SearchOption.AllDirectories);
+
+                for (int i = fileNames.Length - 1; i >= 0; i--)
+                {
+                    musicListPanels = new System.Windows.Forms.Panel[fileNames.Length];
+                    musicListPanelsLabelNames = new System.Windows.Forms.Label[fileNames.Length];
+                    musicListPanelsLabelDurations = new System.Windows.Forms.Label[fileNames.Length];
+                    musicListPanelsButton = new System.Windows.Forms.Button[fileNames.Length];
+                    musicListPanelsCheckBox = new System.Windows.Forms.CheckBox[fileNames.Length];
+
+                    
+
+                    musicListPanels[i] = new System.Windows.Forms.Panel();
+                    panel2.Controls.Add(musicListPanels[i]);
+                    musicListPanels[i].Location = new System.Drawing.Point(0, i*32);
+                    musicListPanels[i].Name = "musicListPanel" + i.ToString();
+                    musicListPanels[i].Size = new System.Drawing.Size(panel2.Width-20, 32);
+                    musicListPanels[i].TabIndex = 1;
+
+                    musicListPanelsCheckBox[i] = new System.Windows.Forms.CheckBox();
+                    musicListPanels[i].Controls.Add(musicListPanelsCheckBox[i]);
+                    musicListPanelsCheckBox[i].Anchor = System.Windows.Forms.AnchorStyles.None;
+                    musicListPanelsCheckBox[i].CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    musicListPanelsCheckBox[i].Location = new System.Drawing.Point(3, 5);
+                    musicListPanelsCheckBox[i].Name = "CheckBox" + i.ToString();
+                    musicListPanelsCheckBox[i].Size = new System.Drawing.Size(25, 24);
+                    musicListPanelsCheckBox[i].TabIndex = 0;
+
+                    musicListPanelsLabelNames[i] = new System.Windows.Forms.Label();
+                    musicListPanels[i].Controls.Add(this.musicListPanelsLabelNames[i]);
+                    musicListPanelsLabelNames[i].Location = new System.Drawing.Point(34, 10);
+                    musicListPanelsLabelNames[i].Name = "label1";
+                    musicListPanelsLabelNames[i].Size = new System.Drawing.Size(172, 13);
+                    musicListPanelsLabelNames[i].TabIndex = 1;
+                    musicListPanelsLabelNames[i].Text = fileNames[i].Substring(fileNames[i].LastIndexOf("\\") + 1);
+
+
+
+                    musicListPanelsButton[i] = new System.Windows.Forms.Button();
+                    musicListPanels[i].Controls.Add(musicListPanelsButton[i]);
+                    musicListPanelsButton[i].BackColor = System.Drawing.Color.Transparent;
+                    musicListPanelsButton[i].BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+                    musicListPanelsButton[i].FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveCaption;
+                    musicListPanelsButton[i].FlatAppearance.BorderSize = 0;
+                    musicListPanelsButton[i].FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                    musicListPanelsButton[i].Font = new System.Drawing.Font("Segoe MDL2 Assets", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    musicListPanelsButton[i].Location = new System.Drawing.Point(211, 1);
+                    musicListPanelsButton[i].Name = i.ToString();
+                    musicListPanelsButton[i].Size = new System.Drawing.Size(30, 30);
+                    musicListPanelsButton[i].TabIndex = 2;
+                    musicListPanelsButton[i].Text = "";
+                    musicListPanelsButton[i].UseVisualStyleBackColor = false;
+                    musicListPanelsButton[i].Click += new System.EventHandler(musicListPanelsButton_Click);
+
+                    musicListPanelsLabelDurations[i] = new System.Windows.Forms.Label();
+                    musicListPanels[i].Controls.Add(musicListPanelsLabelDurations[i]);
+                    musicListPanelsLabelDurations[i].Location = new System.Drawing.Point(247, 10);
+                    musicListPanelsLabelDurations[i].Name = "musicListPanelsLabelNames" + i.ToString();
+                    musicListPanelsLabelDurations[i].Size = new System.Drawing.Size(172, 13);
+                    musicListPanelsLabelDurations[i].TabIndex = 3;
+                    musicListPanelsLabelDurations[i].Text = "00:00";
+
+
+
+                }
+            }
+
+        }
+
+        private void musicListPanelsButton_Click(object sender, EventArgs e)
+        {
+            audioPath = fileNames[Convert.ToInt32((sender as System.Windows.Forms.Button).Name)];
+            if (fileReader == null)
+                fileReader = new AudioFileReader(audioPath);
+            else
+            {
+                fileReader.Dispose();
+                fileReader = new AudioFileReader(audioPath);
+            }
+            if (outputDevice == null)
+                outputDevice = new WaveOutEvent();
+            else
+                outputDevice.Stop();
+            musicTrackBar.Value = 0;
+            musicIsPlaying = false;
+            playButton.Text = "";
+            timerOfPlayback.Enabled = false;
+            outputDevice.Init(fileReader);
+            musicTrackBar.Maximum = fileReader.TotalTime.Minutes * 60 + fileReader.TotalTime.Seconds;
+            maximumDuration.Text = fileReader.TotalTime.Minutes.ToString("00") + ":" + fileReader.TotalTime.Seconds.ToString("00");
+            playButton_Click(null, null);
         }
     }
 }
