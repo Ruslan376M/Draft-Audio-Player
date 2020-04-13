18c58b6a23083f22c84929422d14ca04d9ebab34
Руслан Вальшин <Ruslan376@outlook.com>
2020-04-13 18:23:16 +0500

    Version 2.0

--- AboutWindow.Designer.cs
+++ AboutWindow.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listOfDevelopersLabel = new System.Windows.Forms.Label();
+            this.developersLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // listOfDevelopersLabel
+            // 
+            this.listOfDevelopersLabel.BackColor = System.Drawing.Color.Transparent;
+            this.listOfDevelopersLabel.CausesValidation = false;
+            this.listOfDevelopersLabel.Enabled = false;
+            this.listOfDevelopersLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.listOfDevelopersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listOfDevelopersLabel.ForeColor = System.Drawing.Color.Black;
+            this.listOfDevelopersLabel.Location = new System.Drawing.Point(12, 58);
+            this.listOfDevelopersLabel.Name = "listOfDevelopersLabel";
+            this.listOfDevelopersLabel.Size = new System.Drawing.Size(447, 309);
+            this.listOfDevelopersLabel.TabIndex = 24;
+            this.listOfDevelopersLabel.Text = "Вальшин Руслан:  vk.com/ruslan_376\r\nРапиев Ильдар:  vk.com/id142840387\r\nСафронов " +
+    "Иван:  vk.com/safronovvanya\r\nХусамов Альберт:  vk.com/albertkhusamov";
+            // 
+            // developersLabel
+            // 
+            this.developersLabel.BackColor = System.Drawing.Color.Transparent;
+            this.developersLabel.CausesValidation = false;
+            this.developersLabel.Enabled = false;
+            this.developersLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.developersLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.developersLabel.ForeColor = System.Drawing.Color.Black;
+            this.developersLabel.Location = new System.Drawing.Point(12, 14);
+            this.developersLabel.Name = "developersLabel";
+            this.developersLabel.Size = new System.Drawing.Size(267, 44);
+            this.developersLabel.TabIndex = 23;
+            this.developersLabel.Text = "Разработчики:";
+            this.developersLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // AboutWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listOfDevelopersLabel);
+            this.Controls.Add(this.developersLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AboutWindow";
             this.Text = "AboutWindow";
@@ -43,5 +77,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label listOfDevelopersLabel;
+        private System.Windows.Forms.Label developersLabel;
     }
 }
--- AboutWindow.cs
+++ AboutWindow.cs
@@ -7,6 +7,8 @@ namespace Music_Speed_And_Pitch_Changer
         public void applyTheme()
         {
             this.BackColor = Program.themeControl.firstColor;
+            developersLabel.ForeColor = Program.themeControl.secondColor;
+            listOfDevelopersLabel.ForeColor = Program.themeControl.secondColor;
         }
         public AboutWindow()
         {
